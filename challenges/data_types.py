0a956be8b53a6e422c2c8472159d51084533a672
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-27 13:00:12 -0700

    Update and rename solutions/data_types.py to challenges/data_types.py

---
+++ challenges/data_types.py
@@ -0,0 +1,28 @@
+"""
+Declare 3 variables: one of type int, one of type double, and one of type String.
+Read 3 lines of input from stdin and initialize your variables.
+
+Use the + operator to perform the following operations: 
+Print the sum of i plus your int variable on a new line.
+Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+Concatenate s with the string you read as input and print the result on a new line.
+"""
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + i2)
+
+# Print the sum of the double variables on a new line.
+print(d + d2)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s2)

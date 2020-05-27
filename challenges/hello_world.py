6dc96841877245138ff813c4a23b4aafcbe85cbe
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-27 12:57:21 -0700

    Update hello_world.py

--- challenges/hello_world.py
+++ challenges/hello_world.py
@@ -1,3 +1,8 @@
+"""
+Save a line of input from stdin to a variable, print "Hello, World." on a single line, 
+and finally print the value of your variable on a second line.
+"""
+
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 input_string = input()
 

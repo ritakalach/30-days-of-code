07bdfcd1218dca2613d64228b0d7341f1d739f40
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-05-27 13:18:44 -0700

    Update data_types.py

--- challenges/data_types.py
+++ challenges/data_types.py
@@ -1,4 +1,4 @@
-"""
+'''
 Declare 3 variables: one of type int, one of type double, and one of type String.
 Read 3 lines of input from stdin and initialize your variables.
 
@@ -6,7 +6,7 @@ Use the + operator to perform the following operations:
 Print the sum of i plus your int variable on a new line.
 Print the sum of d plus your double variable to a scale of one decimal place on a new line.
 Concatenate s with the string you read as input and print the result on a new line.
-"""
+'''
 
 i = 4
 d = 4.0

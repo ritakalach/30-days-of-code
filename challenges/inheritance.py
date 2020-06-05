bac6e9242b577cafdadcedd28bb4ee509dde37f0
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-06-05 10:25:25 -0700

    Create inheritance.py

---
+++ challenges/inheritance.py
@@ -0,0 +1,54 @@
+"""
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+Complete the Student class by writing the following:
+
+A Student class constructor, which has 4 parameters:
+1. A string, firsName.
+2. A string, lastName.
+3. An integer, id.
+4. An integer array (or vector) of test scores, scores.
+
+A char calculate() method that calculates a Student object's average 
+and returns the grade character representative of their calculated average.
+"""
+
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super().__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    def calculate(self):
+        
+        average_score = sum(self.scores)/len(self.scores)
+
+        if average_score >= 90:
+            return "O"
+        elif average_score >= 80:
+            return "E"
+        elif average_score >= 70:
+            return "A"
+        elif average_score >= 55:
+            return "P"
+        elif average_score >= 40:
+            return "D"
+        else:
+            return "T"
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())

cc6f258c5bfdfdfc6e6abe9c3b1772a78bf0f434
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-06-09 18:27:12 -0700

    Create exceptions.py

---
+++ challenges/exceptions.py
@@ -0,0 +1,11 @@
+'''
+Read a string, S, and print its integer value; if  cannot be converted to an integer, print "Bad String".
+Note: You must use the String-to-Integer and exception handling constructs built into your submission language.
+'''
+
+S = input().strip()
+
+try:
+    print(int(S))
+except ValueError:
+    print("Bad String")

64f784eb8bad82b4903b69494341c752fc3d90cf
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-06-10 18:19:10 -0700

    Create exceptions_2.py

---
+++ challenges/exceptions_2.py
@@ -0,0 +1,21 @@
+'''
+Write a Calculator class with a single method: int power(int,int). 
+The power method takes two integers, n and p, as parameters and returns the integer result of n^2. 
+If either n or p is negative, then the method must throw an exception with the message: "n and p should be non-negative".
+'''
+
+class Calculator:
+    def power(self, n, p):
+        if n < 0 or p < 0:
+            raise Exception("n and p should be non-negative")
+        return n**p
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   

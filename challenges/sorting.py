e516538988b22a69c92c0702af529938ac608827
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-06-13 13:36:06 -0700

    Create sorting.py

---
+++ challenges/sorting.py
@@ -0,0 +1,8 @@
+'''
+Given an array, a, of size n distinct elements, sort the array in ascending order using the Bubble Sort algorithm. 
+Once sorted, print the following 3 lines:
+  1. "Array is sorted in numSwaps swaps" where numSwaps is the number of swaps that took place.
+  2. "First Element: firstElement" where firstElement is the first element in the sorted array.
+  3. "Last Element: lastElement" where lastElement is the last element in the sorted array.
+'''
+

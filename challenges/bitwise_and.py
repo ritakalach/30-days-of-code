5560d3db5326e277c9ce2de9afa549a521442874
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-06-22 09:05:37 -0700

    Create bitwise_and.py

---
+++ challenges/bitwise_and.py
@@ -0,0 +1,4 @@
+"""
+Given set S={1,2,3,...,N}. Find two integers, A and B (where A<B), from set S such that the value of 
+A&B is the maximum possible and also less than a given integer, K. In this case, & represents the bitwise AND operator.
+"""
